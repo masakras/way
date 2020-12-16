23f7d235a83d1acbc78764f9409438bcec8b2a73
masakras <dmi3case@gmail.com>
2020-12-16 21:43:28 +0300

    wayup

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(window).scroll(function() {
+$('.ambition_title').each(function(){
+var imagePos = $(this).offset().top;
+var topOfWindow = $(window).scrollTop();
+if (imagePos < topOfWindow+650) { $(this).addClass("fadeInLeft"); } }); }); <!-- //////////////////// -->
+    $(window).scroll(function() {
+    $('.benefit_free').each(function(){
+    var imagePos = $(this).offset().top;
+    var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+650) { $(this).addClass("fadeInLeft"); } }); }); <!-- //////////////////// -->
+        $(window).scroll(function() {
+        $('.benefit_travel').each(function(){
+        var imagePos = $(this).offset().top;
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+650) { $(this).addClass("fadeInUp"); } }); }); <!-- //////////////////// -->
+            $(window).scroll(function() {
+            $('.benefit_create').each(function(){
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+650) { $(this).addClass("fadeInRight"); } }); }); <!-- //////////////////// -->
+                $(window).scroll(function() {
+                $('.ticket_title').each(function(){
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow+650) { $(this).addClass("fadeInLeft"); } }); }); <!-- ////////////////////
+                    -->
+                    $(window).scroll(function() {
+                    $('.form').each(function(){
+                    var imagePos = $(this).offset().top;
+                    var topOfWindow = $(window).scrollTop();
+                    if (imagePos < topOfWindow+650) { $(this).addClass("fadeInUp"); } }); });
